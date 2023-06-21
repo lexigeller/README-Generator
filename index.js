307acec820f366b669c7cbfd8ea5410626666b3f
Lexi Geller <leximoconner@yahoo.com>
2023-06-20 21:27:33 -0400

    added initializing function

--- index.js
+++ index.js
@@ -49,7 +49,16 @@ function writeToFile(fileName, data) {
 }
 
 // TODO: Create a function to initialize app
-function init() {}
+function init() {
+    inquirer.prompt(questions)
+      .then((answers) => {
+        const markdown = generateMarkdown(answers);
+        writeToFile('README.md', markdown);
+      })
+      .catch((error) => {
+        console.error(error);
+      });
+  }
 
 // Function call to initialize app
 init();
